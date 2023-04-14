c4fa6b40cd6fd5d2765caab9bae1cd4081ce05f3
marcin10lw <marcin10lw@wp.pl>
2023-04-15 00:33:39 +0200

    initial commit

---
+++ src/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+import normalize from "styled-normalize";
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  html {
+    box-sizing: border-box;
+  }
+
+  *, ::before, ::after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'Nunito Sans', sans-serif;
+  }
+`;
---
+++ src/theme.js
@@ -0,0 +1,22 @@
+const theme = {
+  breakpoints: {
+    mobile: 767,
+  },
+};
+
+export const lightTheme = {
+  ...theme,
+  colors: {
+    textColor: "hsl(200, 15%, 8%)",
+    inputColor: "hsl(0, 0%, 52%)",
+    backgroundColor: "hsl(0, 0%, 98%)",
+    elementsColor: "hsl(0, 0%, 100%)",
+  },
+};
+
+export const darkTheme = {
+  ...theme,
+  textColor: "hsl(0, 0%, 100%)",
+  backgroundColor: "hsl(207, 26%, 17%)",
+  elementsColor: "hsl(209, 23%, 22%)",
+};
