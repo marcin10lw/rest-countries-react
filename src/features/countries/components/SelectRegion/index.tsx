a082f2b2d45749407e750250046bc403cf84bd35
marcin10lw <marcin10lw@wp.pl>
2023-04-21 23:25:12 +0200

    made select window opening logic

--- src/features/countries/components/SelectRegion/index.tsx
+++ src/features/countries/components/SelectRegion/index.tsx
@@ -1,12 +1,42 @@
-import { ArrowWrapper, SelectRegionArrow, StyledSelectRegion } from "./styled";
+import {
+  SelectButton,
+  SelectRegionArrow,
+  SelectWindow,
+  OpenWindowButton,
+  StyledSelectRegion,
+} from "./styled";
+import { useState } from "react";
 
 const SelectRegion = () => {
+  const [isWindowOpen, setIsWindowOpen] = useState(false);
+
   return (
     <StyledSelectRegion>
-      Filter by Region
-      <ArrowWrapper>
+      <OpenWindowButton
+        onClick={() => setIsWindowOpen((isWindowOpen) => !isWindowOpen)}
+      >
+        Filter by Region
         <SelectRegionArrow />
-      </ArrowWrapper>
+      </OpenWindowButton>
+      {isWindowOpen && (
+        <SelectWindow>
+          <li>
+            <SelectButton>Africa</SelectButton>
+          </li>
+          <li>
+            <SelectButton>America</SelectButton>
+          </li>
+          <li>
+            <SelectButton>Asia</SelectButton>
+          </li>
+          <li>
+            <SelectButton>Europe</SelectButton>
+          </li>
+          <li>
+            <SelectButton>Oceania</SelectButton>
+          </li>
+        </SelectWindow>
+      )}
     </StyledSelectRegion>
   );
 };
--- src/features/countries/components/SelectRegion/styled.ts
+++ src/features/countries/components/SelectRegion/styled.ts
@@ -2,31 +2,48 @@ import styled from "styled-components";
 import { ReactComponent as ArrowImage } from "./arrow.svg";
 import { TRANSITION_DURATION, TRANSITION_TIMING } from "../../transition";
 
-export const StyledSelectRegion = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+export const StyledSelectRegion = styled.div`
+  position: relative;
   max-width: 200px;
   width: 100%;
+  box-shadow: 0px 2px 9px rgba(0, 0, 0, 0.0532439);
   background-color: ${({ theme }) => theme.colors.elementsColor};
   color: ${({ theme }) => theme.colors.textColor};
   font-size: 14px;
-  border: none;
   border-radius: 5px;
-  padding: 18px 24px;
   transition-property: background-color, color;
   transition-duration: ${TRANSITION_DURATION};
   transition-timing-function: ${TRANSITION_TIMING};
-  cursor: pointer;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     font-size: 12px;
-    padding: 14px 24px;
     align-self: flex-start;
   }
 `;
 
-export const ArrowWrapper = styled.div`
+export const OpenWindowButton = styled.button`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border: none;
+  padding: 18px 24px;
+  color: ${({ theme }) => theme.colors.textColor};
+  background-color: transparent;
+  cursor: pointer;
+  transition: color ${TRANSITION_DURATION} ${TRANSITION_TIMING},
+    opacity 140ms ease-in-out;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    padding: 14px 24px;
+  }
+
+  &:hover {
+    opacity: 0.75;
+  }
+`;
+
+export const SelectRegionArrow = styled(ArrowImage)`
   width: 12px;
   height: 12px;
 
@@ -36,4 +53,37 @@ export const ArrowWrapper = styled.div`
   }
 `;
 
-export const SelectRegionArrow = styled(ArrowImage)``;
+export const SelectWindow = styled.ul`
+  position: absolute;
+  z-index: 99;
+  background-color: inherit;
+  box-shadow: inherit;
+  border-radius: inherit;
+  width: 100%;
+  padding: 16px 24px;
+  top: 56px;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  cursor: initial;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    top: 46px;
+  }
+`;
+
+export const SelectButton = styled.button`
+  padding: 0;
+  background-color: transparent;
+  color: inherit;
+  border: none;
+  width: 100%;
+  text-align: start;
+  cursor: pointer;
+  transition: opacity 140ms ease-in-out;
+
+  &:hover {
+    opacity: 0.75;
+  }
+`;
