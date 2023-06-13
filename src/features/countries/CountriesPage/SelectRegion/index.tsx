a4c3abc67b06b4095e5aacfa7c3c5d6bef168f68
marcin10lw <marcin10lw@wp.pl>
2023-06-13 23:50:34 +0200

    added closing menu on outside click

--- src/features/countries/CountriesPage/SelectRegion/index.tsx
+++ src/features/countries/CountriesPage/SelectRegion/index.tsx
@@ -1,5 +1,8 @@
-import { motion } from "framer-motion";
+import { useContext, useEffect, useRef, useState } from "react";
+import { CountriesFilterParamsContext } from "../../common/CountriesFilterParamsContext";
 import { listVariants, itemVariants } from "./variants";
+import { motion } from "framer-motion";
+import { regions } from "./regions";
 import {
   SelectButton,
   SelectRegionArrow,
@@ -7,15 +10,27 @@ import {
   OpenWindowButton,
   StyledSelectRegion,
 } from "./styled";
-import { useContext, useState } from "react";
-import { regions } from "./regions";
-import { CountriesFilterParamsContext } from "../../common/CountriesFilterParamsContext";
 
 const SelectRegion = () => {
   const [isWindowOpen, setIsWindowOpen] = useState(false);
   const { region, changeRegion, setCurrentPage } = useContext(
     CountriesFilterParamsContext
   );
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsWindowOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
 
   const windowText = region
     ? regions.find(({ alias }) => region === alias)?.name
@@ -27,12 +42,11 @@ const SelectRegion = () => {
   };
 
   return (
-    <StyledSelectRegion>
+    <StyledSelectRegion ref={menuRef}>
       <OpenWindowButton
         as={motion.button}
         whileTap={{ scale: 0.97 }}
         onClick={() => setIsWindowOpen((isWindowOpen) => !isWindowOpen)}
-        onBlur={() => setIsWindowOpen(false)}
       >
         {windowText}
         <SelectRegionArrow open={isWindowOpen} />
